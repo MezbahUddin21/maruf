f3530ee7e37a52a0e5fc43d23036b5e70fc43110
MezbahUddin21 <78334304+MezbahUddin21@users.noreply.github.com>
2022-09-12 17:48:10 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,11 @@
 !(function($) {
   "use strict";
 
-
+    // preloader
+  var preloader = document.getElementById("preloader");
+    window.addEventListener("load", function(){
+      preloader.style.display="none"
+   });
 
     // Init AOS
     function aos_init() {
